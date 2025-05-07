fd95facdbcc3f3280962f21a2bd8707809b0ec39
yahik <yahiko_pavi@live.com>
2025-05-07 15:35:48 -0300

    Added an option to create and read an account.

--- src/main/java/com/pavi/bank/controller/AccountController.java
+++ src/main/java/com/pavi/bank/controller/AccountController.java
@@ -1,13 +1,34 @@
 package com.pavi.bank.controller;
 
+import com.pavi.bank.model.Account;
+import com.pavi.bank.repository.AccountRepository;
 import lombok.extern.java.Log;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Log
+@RequestMapping("/account")
 public class AccountController {
 
+    private final AccountRepository accountRepository;
 
+    public AccountController(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @PostMapping("/newaccount")
+    public ResponseEntity<Account> createAccount(@RequestBody Account account){
+        Account savedAccount = accountRepository.save(account);
+        return ResponseEntity.ok(savedAccount);
+    }
+
+    @GetMapping("/{accountNumber}")
+    public ResponseEntity<Account> getAccount(@PathVariable String accountNumber) {
+        Account account = accountRepository.findByAccountNumber(accountNumber)
+                .orElseThrow(() -> new RuntimeException("Account not found"));
+        return ResponseEntity.ok(account);
+    }
 
 
 }
